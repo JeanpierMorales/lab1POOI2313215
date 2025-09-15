6b886b2b617bc77ef0251bc1ad22212536a4b417
Jeanpier Morales <142445455+JeanpierMorales@users.noreply.github.com>
2025-09-15 18:06:15 -0500

    Update and rename diez.java to Ejercicio8.java

--- Ejercicio8.java
+++ Ejercicio8.java
@@ -1,4 +1,4 @@
-public class diez {
+public class Ejercicio8 {
     public static void main(String[] args) {
         //10. DADA LA SIGUIENTE SENTENCIA ,ENCONTRAR EL RESULTADO  
         int x=1, y=2, z=3;
