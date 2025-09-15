3a4b923bb8e340cfc243b20a830b5463b870f357
Jeanpier Morales <142445455+JeanpierMorales@users.noreply.github.com>
2025-09-15 18:05:49 -0500

    Update Ejercicio6.java

--- Ejercicio6.java
+++ Ejercicio6.java
@@ -1,4 +1,4 @@
-public class cinco {
+public class Ejercicio6 {
     public static void main(String[] args) {
         int x=1, y=2, z=3; 
         // 5. DADA LA SIGUIENTE SENTENCIA ,ENCONTRAR EL RESULTADO    
