64f51127e7ff0a1b4482647240cf96b90219ac66
Jeanpier Morales <142445455+JeanpierMorales@users.noreply.github.com>
2025-09-15 18:07:18 -0500

    Update and rename ocho.java to Ejercicio11.java

--- Ejercicio11.java
+++ Ejercicio11.java
@@ -1,4 +1,4 @@
-public class ocho {
+public class Ejercicio11 {
     public static void main(String[] args) {
         int x=1, y=2, z=3;
         //8. DADA LA SIGUIENTE SENTENCIA ,ENCONTRAR EL RESULTADO 
